79826249ecbba6101295690d7aafc826d8482cac
Amelia Freeman <ameliakfreeman@gmail.com>
2023-08-14 13:36:25 -0500

    setup React frontend with Flask backend TEMPLATE

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import { Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is an App</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/app.py
@@ -0,0 +1,12 @@
+# Remote library imports
+from flask import request, make_response, session
+from flask_restful import Resource
+import ipdb
+
+# Local imports
+from config import app, db, api
+# Add your model imports
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
