5355552821098ba5a6df7d4136141889f9d83317
Amelia Freeman <ameliakfreeman@gmail.com>
2023-08-16 11:41:57 -0500

    beginning work on user table

--- server/models.py
+++ server/models.py
@@ -5,3 +5,29 @@ from sqlalchemy.orm import validates
 
 
 from config import db, bcrypt
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    email = db.Column(db.String)
+    age = db.Column(db.String)
+
+    _password_hash = db.Column(db.String)
+
+    serialize_rules = ('-_password-hash')
+
+    @property 
+    def password_hash(self):
+        return self._password_hash
+    
+    @password_hash.setter
+    def password_hash(self, new_password_string):
+        byte_object = new_password_string.encode('utf-8')
+        encrypted_hash_object = bcrypt.generate_password_hash(byte_object)
+        hash_object_as_string = encrypted_hash_object.decode('utf-8')
+        self._password_hash = hash_object_as_string
+    
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
