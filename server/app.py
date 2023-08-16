3dabc176ee402e0d0c2413acae391ca5723ab2f6
Amelia Freeman <ameliakfreeman@gmail.com>
2023-08-16 13:43:03 -0500

    initial upgrade, users table upgrade

--- server/app.py
+++ server/app.py
@@ -31,7 +31,41 @@ class Users(Resource):
 
 api.add_resource(Users, '/users')
 
+#LOGIN the User
+@app.route('/login', methods=["POST"])
+def login():
+    data = request.get_json()
 
+    try:
+        user = User.query.filter_by(username=data['username']).first()
+        if user.authenticate(data['password']):
+            session['user.id'] = user.id
+            response = make_response(user.to_dict(), 200)
+            return response
+        else:
+            response = make_response({"errors": "Password incorrect"}, 401)
+            return response
+    except:
+        response = make_response({"errors": "Username incorrect"}, 401)
+        return response
+
+#KEEP the User logged in
+@app.route('/authorized', methods=["GET"])
+def authorize():
+    try:
+        user = User.query.filter_by(id=session.get('user.id')).first()
+        response = make_response(user.to_dict(), 200)
+        return response
+    except:
+        response = make_response({"Error": "User not found."}, 404)
+        return response
+    
+#LOGOUT the User
+@app.route('/logout', methods=["DELETE"])
+def logout():
+    session['user.id']=None
+    response = make_response({}, 204)
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
