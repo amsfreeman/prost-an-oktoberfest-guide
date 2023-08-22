5f48ffbff24b471a5ff5546f09de70bf749f72fb
Amelia Freeman <ameliakfreeman@gmail.com>
2023-08-22 06:18:56 -0500

    work on all visits page

--- server/models.py
+++ server/models.py
@@ -2,7 +2,6 @@ from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.ext.hybrid import hybrid_property, hybrid_method
 from sqlalchemy.orm import validates
-from datetime import datetime
 
 
 from config import db, bcrypt
